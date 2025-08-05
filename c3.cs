f66914962691d1d7db3a70362432236d6146e4a1
Bhaskar Gupta <74544869+chrollolucilfer-v@users.noreply.github.com>
2025-08-05 10:42:33 -0700

    Create c3.cs

---
+++ c3.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program{
+	static void Main(){
+	
+	Console.Write("Enter your degree in celcius");
+	double c = int.Parse(Console.ReadLine());
+		
+		double f = (c*9/5)+32;
+		
+		Console.WriteLine(f);
+		
+		
+		
+	
+	}
+
+}
